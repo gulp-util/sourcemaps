1698aaf55e6baef6f3b3f75b9eb6c0de33ebbe4a
PWall <git@pwall.me>
2023-05-31 17:24:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  init: require('./init'),
+  write: require('./write'),
+  mapSources: require('@gulp-sourcemaps/map-sources'),
+  identityMap: require('@gulp-sourcemaps/identity-map'),
+};
